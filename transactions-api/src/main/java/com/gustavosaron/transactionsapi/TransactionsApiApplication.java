7d0b8a82f394d823bdd4c42ea14fc598f5f01497
Gustavo Moises Saron <gumoises@live.com>
2022-06-24 11:37:35 -0300

    Moved mongodb config to application.yml profile files

--- transactions-api/src/main/java/com/gustavosaron/transactionsapi/TransactionsApiApplication.java
+++ transactions-api/src/main/java/com/gustavosaron/transactionsapi/TransactionsApiApplication.java
@@ -2,8 +2,10 @@ package com.gustavosaron.transactionsapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = "com.gustavosaron.transactionsapi.repository")
 public class TransactionsApiApplication {
 
 	public static void main(String[] args) {
